e0c1a1252b1629d6fdd9084e064e7f93035ebbbd
Tana <finaltruth.shintai@gmail.com>
2019-11-08 02:04:22 +0900

    - コンフィグの構成を細分化。

---
+++ EarliestFuhaRanking/Configurations/FuhaReports.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace EarliestFuhaRanking.Configurations
+{
+    /// <summary>
+    /// フハ レポート出力の設定を格納します。
+    /// </summary>
+    public class FuhaReports
+    {
+        /// <summary>
+        /// フハ レポートの文字エンコーディング名を取得または設定します。
+        /// </summary>
+        public string EncodingName { get; set; } = "";
+
+        /// <summary>
+        /// 詳細フハ レポートのファイル パスを取得または設定します。
+        /// </summary>
+        public string DetailReportFilePath { get; set; } = "";
+
+        /// <summary>
+        /// ツイート用フハ レポートのファイル パスを取得または設定します。
+        /// </summary>
+        public string TweetReportFilePath { get; set; } = "";
+
+        /// <summary>
+        /// フハ レポートの文字エンコーディングを取得します。
+        /// </summary>
+        /// <returns>フハ レポートの文字エンコーディング。</returns>
+        public Encoding GetEncoding()
+        {
+            try
+            {
+                return Encoding.GetEncoding(EncodingName);
+            }
+            catch (ArgumentException ex)
+            {
+                throw new ArgumentException("レポート ファイルの文字エンコーディング名が正しくありません。", ex);
+            }
+        }
+    }
+}
---
+++ EarliestFuhaRanking/Configurations/RankingCollection.cs
@@ -0,0 +1,35 @@
+﻿namespace EarliestFuhaRanking.Configurations
+{
+    /// <summary>
+    /// ランキング集計の設定を格納します。
+    /// </summary>
+    public class RankingCollection
+    {
+        /// <summary>
+        /// 一度のタイムライン問い合わせで取得する最大ツイート数を取得または設定します。
+        /// </summary>
+        public int CountPerQuery { get; set; } = 0;
+
+        /// <summary>
+        /// タイムライン問い合わせを繰り返す回数を取得または設定します。
+        /// </summary>
+        /// <remarks>Twitter APIの仕様上、タイムライン問い合わせは15分あたり15回までです。</remarks>
+        public int MaxQueryIterationCount { get; set; } = 0;
+
+        /// <summary>
+        /// ランキング対象のツイートを抽出する検索ワードを取得または設定します。
+        /// </summary>
+        /// <remarks>部分一致で検索を行い、全角半角や大文字小文字などは区別します。</remarks>
+        public string SearchWord { get; set; } = "";
+
+        /// <summary>
+        /// 順位の基準となるラスボスアカウントの @ID を取得または設定します。
+        /// </summary>
+        public string RankingBaseId { get; set; } = "";
+
+        /// <summary>
+        /// <see cref="RankingCollection"/> の新しいインスタンスを生成します。
+        /// </summary>
+        public RankingCollection() { }
+    }
+}
